098f9e29c4863ada00a9e40024e8b7460035f659
RochaGR <gutop.rocha@gmail.com>
2025-10-21 19:48:52 -0300

    Fix: ExercicioRepository

--- src/main/java/com/senai/projeto/mytraining/repository/ExercicioRepository.java
+++ src/main/java/com/senai/projeto/mytraining/repository/ExercicioRepository.java
@@ -1,7 +1,8 @@
 package com.senai.projeto.mytraining.repository;
 
+import com.senai.projeto.mytraining.model.Exercicio;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExercicioRepository extends JpaRepository<ExercicioRepository, Long> {
+public interface ExercicioRepository extends JpaRepository<Exercicio, Long> {
 
 }
