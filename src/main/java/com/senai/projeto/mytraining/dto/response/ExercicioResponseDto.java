57dc44d4047458be83480f137aef09f3147b2af9
RochaGR <gutop.rocha@gmail.com>
2025-10-28 19:25:43 -0300

    Fix: ExercicioResponseDTO

--- src/main/java/com/senai/projeto/mytraining/dto/response/ExercicioResponseDto.java
+++ src/main/java/com/senai/projeto/mytraining/dto/response/ExercicioResponseDto.java
@@ -1,12 +1,12 @@
 package com.senai.projeto.mytraining.dto.response;
 
-public record ExercicioResponseDTO(
+public record ExercicioResponseDTO (
         Long id,
         String nome,
         Integer series,
         Integer repeticoes,
         Double cargaKg,
         String observacoes,
-        Long treinoId
-) {
+        Long aLong) {
+
 }
